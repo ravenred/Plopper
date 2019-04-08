0afcbac5789c31853d594e9571d11684adad4ad8
Ian O'Connell <ianoconnell01@gmail.com>
2019-04-08 15:36:06 +0100

    Try catch implemented(Bug with If statement)

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
-import datetime
 import re
+
+# timestamp,sig_generator,sig_id,sig_rev,msg,proto,src,srcport,dst,dstport,id,classification,priority
 
 
 def read_file(file_name):
@@ -9,6 +10,8 @@ def read_file(file_name):
 
     for i in start_line[0:]:        # Loops through the file but starts at line 1 to gather data
 
+        print("************ALERT START************")
+
         x = re.search(r'^((?:[0-9]{2}[-\/:.]){5}[0-9]{6})(.*[{]TCP[}]\s*|.*[{]UDP[}]\s*|.*[{]ICMP[}]\s*).(.*)', i)
 
         # print(x.groups())
@@ -16,22 +19,43 @@ def read_file(file_name):
         event = x.group(2).lstrip()
         ip_info = x.group(3)
 
-        try:
-            ip_formatted = re.search(r'\s*(((?:[0-9]{1,3}[.]){1,3}[0-9]{1,3}):([0-9]{1,6}))\s*->\s*(((?:[0-9]{1,3}[.]){1,3}[0-9]{1,3}):([0-9]{1,6}))', ip_info)
-            print(ip_formatted.groups())
+        print(date_time)
+        print(event)
 
+        if event.find("{TCP}") or event.find("{UDP}") == -1:
 
-        except AttributeError:
+            try:
+                ip_formatted = re.search(r'\s*(((?:[0-9]{1,3}[.]){1,3}[0-9]{1,3}):([0-9]{1,6}))\s*->\s*(((?:[0-9]{1,3}[.]){1,3}[0-9]{1,3}):([0-9]{1,6}))', ip_info)
+                # print(ip_formatted.groups())
+                src_ip = ip_formatted.group(2)
+                src_port = ip_formatted.group(3)
+                dest_ip = ip_formatted.group(5)
+                dest_port = ip_formatted.group(6)
+
+                print(src_ip)
+                print(src_port)
+                print(dest_ip)
+                print(dest_port)
+
+            except AttributeError:
+                pass
+
+        else:
+            print("ICMP")
 
             try:
                 icmp_formatted = re.search(r'((?:[0-9]{1, 3}[.]){1, 3}[0-9]{1, 3})\s *->\s * ((?:[0-9]{1, 3}[.]){1, 3}[0-9]{1, 3})', ip_info)
-                print(icmp_formatted.groups())
+                # print(icmp_formatted.groups())
+
+                icmp_src_ip = icmp_formatted.group(1)
+                icmp_dest_ip = icmp_formatted.group(2)
+                print(icmp_src_ip)
+                print(icmp_dest_ip)
 
             except AttributeError:
-                print()
+                pass
 
-        print(date_time)
-        print(event)
+        print("************ALERT END************")
 
     f.close()
 
