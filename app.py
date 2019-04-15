83f02a94d2dc0ef2b0ce530c5a11ffde0b031e87
Ian O'Connell <ianoconnell01@gmail.com>
2019-04-15 16:33:57 +0100

    DataTable, Event & Class Graphs added

--- app.py
+++ app.py
@@ -1,28 +1,87 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import dash_table_experiments as dt
+import dash_table as dt
+import dash_bootstrap_components as dbc
+import plotly.graph_objs as go
 
 
 def app_deploy(plopper_df):
 
-    app = dash.Dash(__name__)
+    src_port_df = plopper_df.groupby(['Source Port']).size().reset_index(name='Count').nlargest(10, 'Count')
+    src_port_df['Source Port'].apply(str)
 
-    app.layout = html.Div([
-        html.H2('My Dash App'),
-        dt.DataTable(
-            id='Plopper Data',
-            rows=plopper_df,
-            editable=False,
-            row_selectable=True,
-            filterable=True,
-            sortable=True,
-            selected_row_indices=[]
-        ),
-        html.Div(id='selected-indexes'),
+    event_df = plopper_df.groupby(['Event']).size().reset_index(name='Count').nlargest(10, 'Count')
+
+    classification_df = plopper_df.groupby(['Classification']).size().reset_index(name='Count').nlargest(10, 'Count')
+
+    app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+    navbar = dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dbc.NavLink("Link", href="#"))
+        ],
+        brand="Plopper",
+        brand_href="#",
+        sticky="top"
+    )
+
+    body = dbc.Container([
+        dbc.Col([
+            html.H2("Dashboard"),
+            dt.DataTable(
+                id="Plopper Data",
+                columns=[{"name": i, "id": i} for i in plopper_df.columns],
+                n_fixed_rows=1,
+                style_cell={'width': '100%'},
+                data=plopper_df.to_dict("rows"),
+
+            )
+        ]),
         dcc.Graph(
-            id='datatable-subplots'
+            id='src_port',
+            figure={
+                'data': [go.Bar(
+                    x=src_port_df['Source Port'],
+                    y=src_port_df['Count'],
+                )
+                ],
+                'layout': go.Layout(
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
+        ),
+        dcc.Graph(
+            id='event',
+            figure={
+                'data': [go.Bar(
+                    x=event_df['Event'],
+                    y=event_df['Count'],
+                )
+                ],
+                'layout': go.Layout(
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
+        ),
+        dcc.Graph(
+            id='classification',
+            figure={
+                'data': [go.Bar(
+                    x=classification_df['Classification'],
+                    y=classification_df['Count'],
+                )
+                ],
+                'layout': go.Layout(
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
         )
-    ], style={'width': '60%'})
+    ])
+
+    app.layout = html.Div([navbar, body])
 
     app.run_server(debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import snort_parser
 import visualizer
 import app
+import webbrowser
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     snort_output_list = snort_parser.read_file('D:/Log File Samples/tg_snort_fast/alerts.fast')  # Reads log to parse
     plopper_df = visualizer.print_df(snort_output_list)
     app.app_deploy(plopper_df)
+    webbrowser.open_new_tab("http://127.0.0.1:8050")
 
 
 if __name__ == "__main__":
