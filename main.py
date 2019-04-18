e18af566965ee228875168ecf5ccab8d94384088
Ian O'Connell <ianoconnell01@gmail.com>
2019-04-18 19:34:44 +0100

    Added Requirements

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import snort_parser
 import visualizer
 import app
 import webbrowser
+import sys
 
 
 def main():
     # print command line arguments
-    snort_output_list = snort_parser.read_file('D:/Log File Samples/tg_snort_fast/alerts.fast')  # Reads log to parse
+    file_location = sys.argv[1]
+    print(file_location)
+    # 'D:/Log File Samples/tg_snort_fast/alerts.fast'
+    snort_output_list = snort_parser.read_file(file_location)  # Reads log to parse
     plopper_df = visualizer.print_df(snort_output_list)
     app.app_deploy(plopper_df)
     webbrowser.open_new_tab("http://127.0.0.1:8050")
